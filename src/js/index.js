36d46da1fc78c8580b09a78abb9ed8e2e11c3cb7
Mark Brand <markbrand@Marks-iMac.fritz.box>
2019-11-11 11:43:22 +0100

    basic functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,75 @@
-const messages = document.querySelectorAll('.message')
+import * as THREE from 'three'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-messages.forEach(element => {
-  element.innerHTML = 'Hello World'
-})
+var container, stats
+var camera, controls, scene, renderer
+var mesh, texture
+var worldWidth = 256,
+  worldDepth = 256
+
+init()
+animate()
+
+function init() {
+  scene = new THREE.Scene()
+  scene.background = new THREE.Color(0x646464)
+
+  stats = new Stats()
+  
+  container = document.getElementById('three-container')
+
+  renderer = new THREE.WebGLRenderer()
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(container.clientWidth, container.clientHeight)
+
+  container.appendChild(renderer.domElement)
+  container.appendChild(stats.dom)
+
+  camera = new THREE.PerspectiveCamera(
+    80,
+    container.clientWidth / container.clientHeight,
+    1,
+    20000
+  )
+
+  camera.position.set(20, 20, 20)
+  camera.lookAt(0, 0, 0)
+
+  controls = new OrbitControls( camera, renderer.domElement )
+  controls.enableDamping = true
+  controls.dampingFactor = 0.025
+  controls.screenSpacePanning = false
+  controls.minDistance = 10;
+	controls.maxDistance = 100;
+
+  let geometry = new THREE.BoxGeometry(10, 10, 10, 10, 10, 10)
+  mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial())
+  scene.add(mesh)
+
+  let directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 ); 
+  scene.add( directionalLight );
+
+  window.addEventListener('resize', onWindowResize, false)
+}
+
+function onWindowResize() {
+  camera.aspect = container.clientWidth / container.clientHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(container.clientWidth, container.clientHeight)
+}
+
+//
+
+function animate() {
+  requestAnimationFrame(animate)
+ 
+  stats.update()
+  controls.update()
+
+  render()
+}
+
+function render() {
+  renderer.render(scene, camera)
+}
